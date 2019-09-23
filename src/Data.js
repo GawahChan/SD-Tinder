a4e3b62ef1605590a5d1fb436462fe21940b7a71
Gawah Chan <gawahchan@gmail.com>
2019-09-23 11:39:44 +0100

    Changed data

--- src/Data.js
+++ src/Data.js
@@ -1,7 +1,7 @@
 const Data = [
-    {id: 0, name: 'Mila Kunis', age: '36', location: 'LA, USA', bio: 'Actress' },
-    {id: 1, name: 'Jennifer Lawrence', age: '29', location: 'Kentucky, USA', bio: 'Actress' },
-    {id: 2, name: 'Emma Watson', age: '29', location: 'Oxford, UK', bio: 'Actress'}
+    {id: 0, name: 'Mila Kunis', img: 'Mila-Kunis', age: '36', location: 'LA, USA', bio: 'Actress' },
+    {id: 1, name: 'Jennifer Lawrence', img: 'Jennifer-Lawrence', age: '29', location: 'Kentucky, USA', bio: 'Actress' },
+    {id: 2, name: 'Emma Watson', img: 'Emma-Watson', age: '29', location: 'Oxford, UK', bio: 'Actress'}
 ];
 
 export default Data;
