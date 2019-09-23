dfdc25a36241c697f91da13d846218ff1cf5335b
Gawah Chan <gawahchan@gmail.com>
2019-09-23 20:34:45 +0100

    changed data

--- src/Data.js
+++ src/Data.js
@@ -1,7 +1,8 @@
 const Data = [
-    {id: 0, name: 'Mila Kunis', img: 'Mila-Kunis', age: '36', location: 'LA, USA', bio: 'Actress' },
-    {id: 1, name: 'Jennifer Lawrence', img: 'Jennifer-Lawrence', age: '29', location: 'Kentucky, USA', bio: 'Actress' },
-    {id: 2, name: 'Emma Watson', img: 'Emma-Watson', age: '29', location: 'Oxford, UK', bio: 'Actress'}
+    {id: 0, name: 'Rosie Mac', img: 'Rosie-Mac', age: '22', location: 'Newcastle, UK', bio: 'Rode 3 dragons, led the Dothraki and commanded the Unsullied. I was Emilia Clarks stunt double for Season 5 Game of Thrones'},
+    {id: 1, name: 'Mila Kunis', img: 'Mila-Kunis', age: '36', location: 'LA, USA', bio: "Actress who loves doing her own stunts! Check out my work from 'Jupiter Ascending' and 'The Spy Who Dumped Me" },
+    {id: 2, name: 'Alicia Vela-Bailey', img: 'Alicia-Vela-Bailey', age: '28', location: 'LA, USA', bio: 'From superheroines to supervillians, I can do a range of stunts. Recently finished doubling for Gal Gadot and am looking for my next gig' },
+    {id: 3, name: 'Jennifer Lawrence', img: 'Jennifer-Lawrence', age: '29', location: 'Kentucky, USA', bio: 'Champion of the 74th Hunger Games! Expert level with a bow and arrow as well as very capable of doing all my own stunts!' },
 ];
 
 export default Data;
