daccd877dcbf5e88a9dd7933ded4de0f210df71d
Gawah Chan <gawahchan@gmail.com>
2019-09-30 16:47:48 +0100

    Updates

---
+++ precache-manifest.017d3f1f4852bb82c5541bf6f3acd130.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "becffad234daa01d8ecdb384decf8ec1",
+    "url": "/SD-Tinder/index.html"
+  },
+  {
+    "revision": "13ee46bce8d531179128",
+    "url": "/SD-Tinder/static/css/main.f572ee5a.chunk.css"
+  },
+  {
+    "revision": "d704127461fcbe61549a",
+    "url": "/SD-Tinder/static/js/2.cca12fd5.chunk.js"
+  },
+  {
+    "revision": "13ee46bce8d531179128",
+    "url": "/SD-Tinder/static/js/main.e1a51df3.chunk.js"
+  },
+  {
+    "revision": "fa2c1db2f52de1fb3b42",
+    "url": "/SD-Tinder/static/js/runtime-main.2d23ca3f.js"
+  },
+  {
+    "revision": "6fa17d179df914243b0308d550e7f66d",
+    "url": "/SD-Tinder/static/media/Alicia-Vela-Bailey.6fa17d17.jpg"
+  },
+  {
+    "revision": "1146b079e953ffb00c69bee36091bb56",
+    "url": "/SD-Tinder/static/media/Bobby-Hanton.1146b079.jpg"
+  },
+  {
+    "revision": "f0f4194afc664ce4092eee347dda375a",
+    "url": "/SD-Tinder/static/media/Damien-Bryson.f0f4194a.jpg"
+  },
+  {
+    "revision": "6d62b41b223c2a8253170d39f8956df7",
+    "url": "/SD-Tinder/static/media/Janene-Carleton.6d62b41b.jpg"
+  },
+  {
+    "revision": "5562cb1505b4fc87bfef3b41484abec7",
+    "url": "/SD-Tinder/static/media/Katie-Eischen.5562cb15.jpg"
+  },
+  {
+    "revision": "11f27be6efc9722a7f3cdab54a08275e",
+    "url": "/SD-Tinder/static/media/Renae-Moneymaker.11f27be6.jpg"
+  },
+  {
+    "revision": "e05df443e494135c9b1c607e759c6853",
+    "url": "/SD-Tinder/static/media/Richard-Cetrone.e05df443.jpg"
+  },
+  {
+    "revision": "c28d9926e867c1d8a6e72440d537e52a",
+    "url": "/SD-Tinder/static/media/Rosie-Mac.c28d9926.jpg"
+  },
+  {
+    "revision": "9708eb19c5a9fcb7f390b328a2d29f54",
+    "url": "/SD-Tinder/static/media/Tanoai-Reed.9708eb19.jpg"
+  },
+  {
+    "revision": "bedf617457c5eb3de193c5c6cf2fe550",
+    "url": "/SD-Tinder/static/media/disliked.bedf6174.svg"
+  },
+  {
+    "revision": "b9fba64e0cda0b99ab4ae66f237a4c23",
+    "url": "/SD-Tinder/static/media/info.b9fba64e.svg"
+  },
+  {
+    "revision": "5a433b84db1758f73a1181036b045e5d",
+    "url": "/SD-Tinder/static/media/liked.5a433b84.svg"
+  },
+  {
+    "revision": "b5e8710ba4078de63c4c605417d4f09d",
+    "url": "/SD-Tinder/static/media/superliked.b5e8710b.svg"
+  }
+]);
