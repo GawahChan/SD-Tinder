4b5f27e18648d49f26d8299217ae9e871a7d4750
Gawah Chan <gawahchan@gmail.com>
2019-11-19 11:50:59 +0000

    Changed index.js to GlobalStyle

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { GlobalStyle } from "./App.styles";
+import Deck from "./Components/Deck/Deck";
+
+function App() {
+  return (
+    <div>
+      <GlobalStyle />
+      <Deck />
+    </div>
+  );
+}
+
+export default App;
