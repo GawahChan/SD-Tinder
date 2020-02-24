01db9d6b4cb86d02f1b19f162cef5fe0055054fe
Gawah Chan <gawahchan@gmail.com>
2020-02-24 21:43:09 +0000

    Changed BrowserRouter to HashRouter

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { HashRouter as Router, Switch, Route } from "react-router-dom";
 import { HOME_URL, DECK_URL } from "./Constants";
 
 import { GlobalStyle } from "./App.styles";
